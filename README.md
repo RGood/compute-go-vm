e521d8d8e5a031418a75f0dc1a6849c38b25398f
RGood <mullan17@gmail.com>
2023-05-20 16:24:20 -0400

    Comments

--- README.md
+++ README.md
@@ -11,6 +11,10 @@ Quick example of instantiating a gRPC Go service in a custom container at runtim
 1. Run `make build-worker`
 2. Run `make run`
 
+## Notes
+
+* Need to run `go run cmd/main/main.go` as `root` or else it won't have the necessary permissions to bind to the unix socket made by the VM
+
 ## TODO
 
 * Clean up unix socket after container termination
--- cmd/main/main.go
+++ cmd/main/main.go
@@ -13,7 +13,7 @@ import (
 	"google.golang.org/grpc"
 )
 
-func createMachine(c *cluster.Cluster, id string) *cluster.Machine {
+func createMachine(c *cluster.Cluster, id string, backend string) *cluster.Machine {
 	socketPath := fmt.Sprintf("/tmp/compute/%s", id)
 
 	m := c.NewMachine(&config.Machine{
@@ -21,7 +21,7 @@ func createMachine(c *cluster.Cluster, id string) *cluster.Machine {
 		Image:      "docker.io/library/compute:worker01",
 		Privileged: true,
 		PublicKey:  "machine-key",
-		Backend:    "docker",
+		Backend:    backend,
 		Volumes: []config.Volume{
 			{
 				Type:        "bind",
@@ -43,7 +43,7 @@ func dial(addr string, t time.Duration) (net.Conn, error) {
 }
 
 func main() {
-	// Create unix socket
+	// Create cluster under which to build containers
 	c, err := cluster.New(config.Config{
 		Cluster: config.Cluster{
 			Name: "compute",
@@ -53,25 +53,34 @@ func main() {
 		panic(err)
 	}
 
+	// Create a key store (required for creating containers)
 	s := cluster.NewKeyStore("/tmp/keystore")
 	s.Init()
 
+	// Store a dummy public key
 	err = s.Store("machine-key", "machine-key")
 
+	// Set the container's key store to the one we made
 	c.SetKeyStore(s)
 
+	// Create a custom machine ID
 	id := uuid.NewString()
-	m := createMachine(c, id)
+
+	// Create machine
+	m := createMachine(c, id, "docker")
 	defer c.DeleteMachine(m, 0)
 
+	// Connect to the gRPC socket created by the machine
 	conn, err := grpc.Dial(fmt.Sprintf("/tmp/compute/%s/socket.sock", id), grpc.WithInsecure(), grpc.WithDialer(dial))
 	if err != nil {
 		println(err.Error())
 		return
 	}
 
+	// Instantiate an echo client using that connection
 	echoClient := echo.NewEchoClient(conn)
 
+	// Call the echo client N times and verify it succeeded
 	for i := 0; i < 10; i++ {
 		res, err := echoClient.Ping(context.Background(), &echo.Message{
 			Message: fmt.Sprintf("foo: %d", i),
